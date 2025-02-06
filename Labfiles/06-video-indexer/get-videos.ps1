df6345d26438c54e4ee6dfe05bc63730e10df0a8
ManvithGopu13 <manvithgopu1394@gmail.com>
2025-02-06 20:07:59 +0530

    Completed video indexing

--- Labfiles/06-video-indexer/get-videos.ps1
+++ Labfiles/06-video-indexer/get-videos.ps1
@@ -1,5 +1,5 @@
-$account_id="YOUR_ACCOUNT_ID"
-$api_key="YOUR_API_KEY"
+$account_id="571172c6-9573-4e57-b743-a94ef23d159a"
+$api_key="488632b9999b4311aaefa0e7386f22a9"
 $location="trial"
 
 # Call the AccessToken method with the API key in the header to get an access token
